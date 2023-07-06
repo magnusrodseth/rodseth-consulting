d6a7e2c7009fc8ae2a0587437dfc0acb01985da1
magnusrodseth <magnus.rodseth@gmail.com>
2023-07-06 22:10:32 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/edge";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  site: "https://www.rodsethconsulting.com",
 });
